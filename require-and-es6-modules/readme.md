03c00715cf6bd48b6416140cfbf8a67152a317f5
furkanisitan <furkanisitan96@gmail.com>
2020-12-28 06:04:07 +0300

    add require-and-es6-modules

---
+++ require-and-es6-modules/readme.md
@@ -0,0 +1,1 @@
+require and module concept.
---
+++ require-and-es6-modules/src/module1.js
@@ -0,0 +1,56 @@
+// CommonJs Modules
+/*
+// en son export geçerlidir
+module.exports.test1 = function test1(){
+    console.log("Test 1");
+}
+
+module.exports.test2 = function test2(){
+    console.log("Test 2");
+}
+*/
+
+/*
+// birden fazla export
+module.exports = {
+    name: "test name",
+    test1: function (){
+        console.log("Test 1");
+    },
+    person: {
+        name:"test name",
+        mail:"test@gmail.com"
+    }
+
+}
+*/
+//ES6 Modules
+
+// export const name = "test name";
+// export function test() {
+//     console.log("Test Fonksiyonu");
+// }
+// export class Person {
+//     static Test() {
+//         console.log("Person Test");
+//     }
+// }
+
+// export const employee = {
+//     name: "test name",
+//     salary: 4000
+// }
+
+// // default exports
+
+// export default class Deneme {
+//     static deneme() {
+//         console.log("Default Deneme");
+//     }
+// }
+
+// const denemeVal = "Deneme Değeri";
+
+// // . . .
+
+// export default denemeVal;
